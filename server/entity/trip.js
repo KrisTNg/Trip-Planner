1c529721549cd00bdee9361bda4fa92b85672106
QUANG TRUONG <TRUONGCHIQUANG@GMAIL.COM>
2022-07-01 12:59:12 -0500

    install mongoose

---
+++ server/entity/trip.js
@@ -0,0 +1,4 @@
+export class trip{
+    id: number;
+
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-// server/index.js
 
 const express = require('express');
+const mongoose = require('mongoose');//import mongoose module
 
 const PORT = process.env.PORT || 3001;
 
@@ -13,3 +13,13 @@ app.get('/api', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+//============================ MongoDB connection
+//setup default mongoose connection
+const mongoDB = '';
+mongoose.connect(mongoDB,{useNewUrlParser:true, 
+                          useFindAndModify: false, 
+                          useUnifiedTopology:true});
+
+const db = mongoose.connection;//get default connection
+db.on('error', console.error.bind(console,'MongoDB connection error'));//Bind connection to error event
