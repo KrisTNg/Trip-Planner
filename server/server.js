e36195f598d3dac69f3b2ac09afd6f6211991be5
QUANG TRUONG <TRUONGCHIQUANG@GMAIL.COM>
2022-07-01 13:29:37 -0500

    Update server.js
    Update connection to MongoDb

--- server/server.js
+++ server/server.js
@@ -16,10 +16,19 @@ app.listen(PORT, () => {
 
 //============================ MongoDB connection
 //setup default mongoose connection
-const mongoDB = '';
-mongoose.connect(mongoDB,{useNewUrlParser:true, 
-                          useFindAndModify: false, 
-                          useUnifiedTopology:true});
+const username = "admin";
+const password = "admin";
+const cluster = "cluster0";
+const dbname = "";
+
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.pp3gy.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+  { 
+    useNewUrlParser:true, 
+    useUnifiedTopology:true
+  });
 
 const db = mongoose.connection;//get default connection
 db.on('error', console.error.bind(console,'MongoDB connection error'));//Bind connection to error event
+db.once("open", function () {
+  console.log("Connected successfully");
+});
