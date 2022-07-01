22a114e1fae0b9da8fe86a9e2a6e71d70c787f2a
QUANG TRUONG <TRUONGCHIQUANG@GMAIL.COM>
2022-07-01 13:48:02 -0500

    create entity trip and route for testing push-get to mongoDB

--- server/entity/trip.js
+++ server/entity/trip.js
@@ -1,4 +1,28 @@
-export class trip{
-    id: number;
+const mongoose = require("mongoose");
 
-}
+const TripSchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true
+    },
+    name:{
+        type: String
+    },
+    date:{
+        type: Date
+    },
+    description:{
+        type: String
+    },
+    owner_id:{
+        type:Number
+    },
+    user_id_list:{
+        type:Number,
+        default : []
+    }
+});
+
+const Trip = mongoose.model("Trip", TripSchema);
+
+module.exports = Trip;
