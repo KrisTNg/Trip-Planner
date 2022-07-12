310c885c4ff971f3379922139175b1fc801faae7
QUANG TRUONG <TRUONGCHIQUANG@GMAIL.COM>
2022-07-11 20:40:52 -0500

    Update routes.js
    add replacement for body-parse

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,10 @@ const express = require("express");
 const tripModel = require("../server/models/trip")
 const app = express();
 const bodyParser = require("body-parser");
+/*
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+*/
 
 app.use(bodyParser.json());
 
